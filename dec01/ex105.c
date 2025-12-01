03d8de2c33392813cbeb40abc0869bd199066f24
xericue <106630482+xericue@users.noreply.github.com>
2025-12-01 14:33:11 +0000

    ex105.c

---
+++ dec01/ex105.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    // pros of an array are that you can access any element. index-typed data structure
+
+    // other data structures
+    // STACK!!! (you have to create this yourself in C)
+    // // put things on top - first in is first out (pancakes)
+
+    return 0;
+}
