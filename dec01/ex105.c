6ea7dd989db1b7368143ee5bcdef8fb3a8f072af
xericue <106630482+xericue@users.noreply.github.com>
2025-12-01 14:47:01 +0000

    first recursive function - hello n until n > 10

--- dec01/ex105.c
+++ dec01/ex105.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
 
+void func(int);
+
 int main()
 {
     // pros of an array are that you can access any element. index-typed data structure
 
     // other data structures
     // STACK!!! (you have to create this yourself in C)
-    // // put things on top - first in is first out (pancakes)
+        // put things on top - last in is first out (pancakes, LIFO)
+        // lifo is also how functions get implemented - calling a function is like adding pancakes
+        // like recursion... heheheh...
+
+    // QUEUE!!! first in is first out (supermarket line, FIFO)
+
+    // what happens in recursion?
+        // a copy of func gets pushed to the stack
+            // if no base case: go "to the point right after the call to func in the last copy"
+                // i.e. each copy of func has to get released first before the previous.
+                // LIFO. backtracing of recursion.
+    int n = 1;
+
+    func(n);
 
     return 0;
+}
+
+void func(int n) {
+    if (n > 10) return;
+    printf("hello %d\n", n);
+    func(n + 1); // modified parameter at the call
 }
